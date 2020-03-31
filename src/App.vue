a9433a73af8900a01cdaebce1cf8bc36d4b5b1c3
Md. Ruhul Amin Khan <39692985+ruhulite@users.noreply.github.com>
2020-03-31 22:31:55 +0600

    environment setup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Tictactoe msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Tictactoe from './components/Tictactoe.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tictactoe
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tictactoe.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h2>A simple Tic Tac Toe</h2>
+    <div class="game--container">
+      <div data-cell-index="0" class="cell"></div>
+      <div data-cell-index="1" class="cell"></div>
+      <div data-cell-index="2" class="cell"></div>
+      <div data-cell-index="3" class="cell"></div>
+      <div data-cell-index="4" class="cell"></div>
+      <div data-cell-index="5" class="cell"></div>
+      <div data-cell-index="6" class="cell"></div>
+      <div data-cell-index="7" class="cell"></div>
+      <div data-cell-index="8" class="cell"></div>
+    </div>
+    <h2 class="game--status"></h2>
+    <button class="game--restart">Restart Game</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../assets/css/style.css';
+</style>
